4c75c2eba328996f83b8c89d24548d8c8d5391e3
Mads Kristian Hansen <mads_hansen@hotmail.de>
2022-01-13 16:07:15 +0100

    Dialog-edit-user & address added

---
+++ src/app/dialog-edit-user/dialog-edit-user.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { User } from 'src/models/user.class';
+
+@Component({
+  selector: 'app-dialog-edit-user',
+  templateUrl: './dialog-edit-user.component.html',
+  styleUrls: ['./dialog-edit-user.component.scss']
+})
+export class DialogEditUserComponent implements OnInit {
+
+  constructor(public dialogRef: MatDialogRef<DialogEditUserComponent>) { }
+
+  loading: boolean = false;
+  user!: User;
+  birthDate!: Date;
+
+  ngOnInit(): void {
+  }
+
+  saveUser() {
+
+  }
+
+}
--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute } from '@angular/router';
 import { User } from 'src/models/user.class';
+import { DialogEditAddressComponent } from '../dialog-edit-address/dialog-edit-address.component';
+import { DialogEditUserComponent } from '../dialog-edit-user/dialog-edit-user.component';
 
 @Component({
   selector: 'app-user-detail',
@@ -13,7 +16,7 @@ export class UserDetailComponent implements OnInit {
   userId: any = '';
   user: User = new User();
 
-  constructor(private route: ActivatedRoute, private firestore: AngularFirestore) { }
+  constructor(private route: ActivatedRoute, private firestore: AngularFirestore, public dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe( paramMap => {
@@ -35,11 +38,13 @@ export class UserDetailComponent implements OnInit {
   }
 
   editUserDetail() {
-
+    const dialog = this.dialog.open(DialogEditUserComponent);
+    dialog.componentInstance.user = this.user;
   }
 
   editMenu() {
-
+    const dialog = this.dialog.open(DialogEditAddressComponent);
+    dialog.componentInstance.user = this.user;
   }
 
 }
